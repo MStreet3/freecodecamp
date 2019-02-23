279fa33c1569a8d113b9604cc7571cc37d510f3f
Michael Street <mi.street@gmail.com>
2019-02-23 17:19:34 -0500

    Update telephoneCheck.js

--- src/algs-data-structures/telephoneCheck.js
+++ src/algs-data-structures/telephoneCheck.js
@@ -1,5 +1,6 @@
 function telephoneCheck(str) {
-  return str;
+  let phoneRegex = /^1?(\s|-)?(\({1}\d{3}\){1}|\d{3})(\s|-)?(\d{3})(\s|-)?(\d{4})$/g;
+  return phoneRegex.test(str);
 }
 
 export { telephoneCheck };
