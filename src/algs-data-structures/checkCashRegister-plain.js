3433723b7a91fe349c2bfffd37c0563d39d55ed4
Michael Street <mi.street@gmail.com>
2019-04-04 16:13:17 -0400

    style changes

--- src/algs-data-structures/checkCashRegister-plain.js
+++ src/algs-data-structures/checkCashRegister-plain.js
@@ -4,8 +4,8 @@ function totalFunds(cid) {
   }, 0);
 }
 
-function setupDrawer() {
-  const denominations = [
+function setupDrawer(
+  denominations = [
     ['PENNY', 0.01],
     ['NICKEL', 0.05],
     ['DIME', 0.1],
@@ -15,34 +15,33 @@ function setupDrawer() {
     ['TEN', 10],
     ['TWENTY', 20],
     ['ONE HUNDRED', 100]
-  ];
-  const drawer = denominations.reduce((drawer, denomination) => {
-    let [billOrCoin, unitValue] = denomination;
-    drawer[billOrCoin] = {
-      unitValue: unitValue,
-      total: 0,
-      count: 0
-    };
-    return drawer;
-  }, {});
+  ]
+) {
   return {
-    drawer: drawer,
+    drawer: denominations.reduce((drawer, denomination) => {
+      let [billOrCoin, unitValue] = denomination;
+      drawer[billOrCoin] = {
+        unitValue: unitValue,
+        total: 0,
+        count: 0
+      };
+      return drawer;
+    }, {}),
     denominations: denominations
   };
 }
 
 function fillDrawer(cid) {
   const initial = setupDrawer();
-  const filledDrawer = cid.reduce((drawer, denomination) => {
-    let [billOrCoin, totalValue] = denomination;
-    let count = Math.round(totalValue / drawer[billOrCoin]['unitValue']);
-    drawer[billOrCoin]['total'] = totalValue;
-    drawer[billOrCoin]['count'] = count;
-    return drawer;
-  }, initial.drawer);
   return {
     initial: initial.drawer,
-    filled: filledDrawer,
+    filled: cid.reduce((drawer, denomination) => {
+      let [billOrCoin, totalValue] = denomination;
+      let count = Math.round(totalValue / drawer[billOrCoin]['unitValue']);
+      drawer[billOrCoin]['total'] = totalValue;
+      drawer[billOrCoin]['count'] = count;
+      return drawer;
+    }, initial.drawer),
     sortedDenominations: initial.denominations
       .sort((a, b) => {
         return b[1] - a[1];
@@ -56,17 +55,16 @@ function fillDrawer(cid) {
 
 function countOutChange(change, cid) {
   const register = fillDrawer(cid);
-
   return register.sortedDenominations
     .map((denomination) => {
       let available = register.filled[denomination]['count'];
       let unitValue = register.filled[denomination]['unitValue'];
-      let remainder = Math.round((change % unitValue + 0.000001) * 100) / 100;
+      let remainder = Math.round((change % unitValue + 0.0000001) * 100) / 100;
       let count = Math.round((change - remainder) / unitValue);
       if (count > 0) {
         let countValue =
           count >= available ? available * unitValue : count * unitValue;
-        change = Math.round((change - countValue + 0.000000001) * 100) / 100;
+        change = Math.round((change - countValue + 0.0000001) * 100) / 100;
         return [denomination, countValue];
       }
     })
@@ -92,6 +90,9 @@ function checkCashRegister(price, cash, cid) {
       return { status: status, change: cid };
     default:
       res = countOutChange(change, cid);
+      // check for situations where cashOnHand > change
+      // but insufficient denominations to return the required change
+      // e.g., change = 0.96 yet the register has only a single dollar bill.
       return totalFunds(res) < change
         ? {
             status: 'INSUFFICIENT_FUNDS',
